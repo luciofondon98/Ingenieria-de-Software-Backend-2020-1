13bf4276e77e6b925accc560f4ba60307f0c0b8a
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-08-08 22:51:47 -0400

    adding imports for cross

--- src/main/java/rest_api/controller/PacienteController.java
+++ src/main/java/rest_api/controller/PacienteController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import rest_api.entity.Paciente;
 import rest_api.service.PacienteService;
 
